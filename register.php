6871dacd749ea38c643ceac48d99aa5afe3ba8ef
cschlotzhauer <cschlotzhauer@gsu.edu>
2014-07-20 15:36:02 -0400

    Update register.php

--- register.php
+++ register.php
@@ -1,11 +1,69 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN"
-   "http://www.w3.org/TR/html4/strict.dtd">
 <?php
+	
+	session_start();
+	
+	$errortitle="";
+	$errorname="";
+	
+	
+	
+	if(!empty($_POST['submitted'])){
+		include('connect.php');
+		mysql_select_db("blog_db");
+		$username = $_POST['username'];
+		$password = $_POST['pass'];
+		$blogtitle = $_POST['title'];
+		$password = crypt($password);
+		$toggle=true;
 
-session_start();
+		
+		$get_users = mysql_query("SELECT * FROM tbl_users where username = '$username'");
+		if($get_users){
+			$row=mysql_fetch_array($get_users);
+			if(!empty($row)){
+				$errorname='Sorry, the username' . $username . ' already exists. </br>';
+				$toggle=false;
+			}
+		}
+		
 
-?>
+		$get_blogs = mysql_query("SELECT * FROM tbl_users where blog_title = '$blogtitle'");
+		if($get_blogs){
+			$row=mysql_fetch_array($get_blogs);
+			if(!empty($row)){
+				$errortitle='Sorry, a Blog called ' . $username . ' already exists. </br>';
+				$toggle=false;
+			}
+		}
+		
+		if($toggle){
+			
 
+
+
+		mysql_query("INSERT into tbl_users (f_name, l_name, username, password, blog_title) VALUES('".$_POST['fname']."','".$_POST['lname']."','".$username."','".$password."','".$blogtitle."')") or die(mysql_error());
+
+		//remove this line if the create trigger works in mysql.
+		//mysql_query("INSERT into tbl_blogs (title, username) VALUES('".$_POST['title']."','".$_POST['username']."')") or die(mysql_error());
+		$result=mysql_query("SELECT blog_ID FROM tbl_blogs WHERE username='$username'");
+		$row=mysql_fetch_array($result);
+
+		$_POST['username'] = stripslashes($_POST['username']);
+		$hour = time() + 3600;
+		setcookie('id_site', $_POST['username'], $hour);
+		setcookie('key_site', $_POST['pass'], $hour);
+		$_SESSION['username']=$_POST['username'];
+		//closes mysql
+		mysql_close($link);
+
+		//redirects user to index page to log in
+		$redirect="Location: BlogPage.php?blog_ID=".$row[0];
+		header($redirect);
+		exit;
+		}
+	}
+
+?>	
 <html lang="en">
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
@@ -13,13 +71,15 @@ session_start();
 	<meta name="generator" content="TextMate http://macromates.com/">
 	<meta name="author" content="Vera">
 	<!-- Date: 2014-07-18 -->
-	
+
 
 </head>
 <body>
-	
+
 	<!--Registration Form-->
 	<h2>Registration Form</h2>
+	<?php echo $errorname ?>
+	<?php echo $errortitle ?>
 	<form id='register' action='register.php' method='post'
 	    accept-charset='UTF-8' onsubmit="return validateFormr()">
 	<fieldset >
@@ -39,53 +99,5 @@ session_start();
 	</fieldset>
 	</form>
 	<a href="index.php"> Return to Index </a>
-	
-	
-	<?php
-	include('connect.php');
-	mysql_select_db("blog_db");
-	$username = $_POST['username'];
-	$password = $_POST['pass'];
-	$blogtitle = $_Post['title'];
-	//encrypt password at entry with crypt()
-	//$password = crypt($password);
-	
-
-	$get_users = mysql_query("SELECT * FROM tlb_users where username = '$username'");
-	$get_rows = mysql_affected_rows($get_users);
-	if($get_rows!=0){
-		die('Sorry, the username' . $username . ' already exists.');
-	}
-
-
-	$get_blogs = mysql_query("SELECT * FROM tlb_users where blog_title = '$blogtitle'");
-	$get_rows = mysql_affected_rows($get_blogs);
-	if($get_rows!=0){
-		die('Sorry, the title' . $blogtitle . ' is already in use.');
-	}
-
-
-
-mysql_query("INSERT into tbl_users (f_name, l_name, username, password, blog_title) VALUES('".$_POST['fname']."','".$_POST['lname']."','".$_POST['username']."','".$_POST['pass']."','".$_POST['title']."')") or die(mysql_error());
-
-//remove this line if the create trigger works in mysql.
-mysql_query("INSERT into tbl_blogs (title, username) VALUES('".$_POST['title']."','".$_POST['username']."')") or die(mysql_error());
-
-
-	$_POST['username'] = striplashes($_POST['username']);
-	$hour = time() + 3600;
-	setcookie('id_site', $_POST['username'], $hour);
-	setcookie('key_site', $_POST['pass'], $hour);
-
-	//closes mysql
-	mysql_close($link);
-
-	//redirects user to index page to log in
-	header("Location: members.php");
-	exit;
-
-
-	?>	
-	
 </body>
 </html>
