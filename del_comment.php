f64006d2e164d3e2e5f0a21e6ef93101baee40bb
veram91 <veram91@gmail.com>
2014-07-20 20:09:24 -0400

    Create del_comment.php
    Deletes the comment but does not redirect either...Idk what I'm doing wrong

---
+++ del_comment.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN"
+   "http://www.w3.org/TR/html4/strict.dtd">
+
+<?php
+session_start();
+if(isset($_COOKIE['username'])){
+	$_SESSION['username']=$_COOKIE['username'];
+}
+include('connect.php');
+mysql_select_db('blog_db');
+$blognum = $_POST['blog_ID'];
+$entry_ID = $_POST['entry_ID'];
+$author = $_POST['c_author'];
+$content = $_POST['c_content'];
+$date = $_POST['c_date'];
+
+$query = "DELETE FROM tbl_comments WHERE entry_ID = '$entry_ID' 
+									AND author = '$author' 
+									AND content = '$content' 
+									AND date = '$date'";
+									
+mysql_query($query);
+header("Location: get_comments.php?blog_ID=".$blognum."&entry_ID=".$entryid);
+
+
+
+?>
+
+
+
+<html lang="en">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<title>untitled</title>
+	<meta name="generator" content="TextMate http://macromates.com/">
+	<meta name="author" content="Vera">
+	<!-- Date: 2014-07-20 -->
+</head>
+<body>
+
+</body>
+</html>
