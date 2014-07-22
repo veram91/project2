a760a2b62398ec82e6731569a09a21a510f3a173
cschlotzhauer <cschlotzhauer@gsu.edu>
2014-07-22 19:40:03 -0400

    Update NewPost.php
    cleaning up the apostrophe entry error I made that crashes the blog

--- NewPost.php
+++ NewPost.php
@@ -17,6 +17,16 @@ if(empty($_POST['blog_ID'])){
 $blog_ID=$_POST['blog_ID'];
 }
 
+if(!empty($_POST['title'])){
+$_SESSION['title']=$_POST['title'];
+}
+
+if(!empty($_POST['content'])){
+$_SESSION['content']=$_POST['content'];
+}
+
+
+
 //redirect if quit button is pressed
 if(!empty($_POST['quit'])){
  header ("Location: BlogPage.php?blog_ID=$blog_ID");
@@ -26,8 +36,8 @@ if(!empty($_POST['quit'])){
 if(!empty($_POST['post'])){
 	$con=mysqli_connect($db_host, $db_user, $db_pass, $db_db);
 	$username = mysqli_real_escape_string($con, $_SESSION['username']);
-	$entrytitle= mysqli_real_escape_string($con, $_POST['title']);
-	$entry= mysqli_real_escape_string($con, $_POST['content']);
+	$entrytitle= mysqli_real_escape_string($con, $_SESSION['title']);
+	$entry= mysqli_real_escape_string($con, $_SESSION['content']);
 		if (mysqli_connect_errno()) {
 			echo "Failed to connect to MySQL: " . mysqli_connect_error();
 		}
@@ -36,34 +46,40 @@ if(!empty($_POST['post'])){
 	if (!mysqli_query($con,$query)) {
 		die('Error: ' . mysqli_error($con));
 	}
+	$_SESSION['title']="";
+	$_SESSION['content']="";
 	$redirect="Location: BlogPage.php?blog_ID=".$blog_ID;
 	header($redirect);
 }
 
 
-if(!empty($_POST['title'])&&!empty($_POST['content'])){
+if(!empty($_POST['rewrite'])||!empty($_POST['preview'])){
 	if(empty($_POST['rewrite'])){ //code to show a preview of the post
-	$title=$_POST['title'];
-	$content=$_POST['content'];
+	$title=$_SESSION['title'];
+	$content=$_SESSION['content'];
 	$content=nl2br($content);
 
 	$preview="<div class='entry'>
 		<div class='etitle'><h2>$title</h2></div>
 		<div class='etext'>$content</div></br></div>";
+	$title=addslashes($title);
+	$content=addslashes($content);	
+	$_SESSION['title']=$title;
+	$_SESSION['content']=$content;
 
 	$form="
 	<form name='newpost' action='NewPost.php' method='post'>
 	<input type='hidden' name='blog_ID' value='$blog_ID'></input>
-	<input type='hidden' name='title' value='$title'></input>
-	<input type='hidden' name='content' value='$content'></input>
 	<input type='submit' name='post' value='Publish post'>
 	<input type='submit' name='rewrite' value='Rewrite'>
 	<input type='submit' name='quit' value='Quit without posting'>
 	</form>";
 	}else{ //code to edit the post after preview
-	$title=$_POST['title'];
-	$content=$_POST['content'];
+	$title=$_SESSION['title'];
+	$title=stripslashes($title);
+	$content=$_SESSION['content'];
 	$content=str_replace("<br />", "", $content);
+	$content=stripslashes($content);
 	$form="
 	<form name='newpost' action='NewPost.php' method='post'>
 	<label class='posttitle'>Post Title:</label></br>
