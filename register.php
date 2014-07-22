998fc9b72b8d515322d697e6804211b0dc3d97ec
suddin7 <suddin7@users.noreply.github.com>
2014-07-22 13:58:22 -0400

    Update register.php
    added period in text

--- register.php
+++ register.php
@@ -80,7 +80,7 @@
 
 	<!--Registration Form-->
 	<h1 class="logoh1">BlogNow...</h1>
-	<h4 class="logoh4">Who will YOU influence today? Make a difference NOW</h4>
+	<h4 class="logoh4">Who will YOU influence today? Make a difference NOW.</h4>
 	<?php echo $errorname ?>
 	<?php echo $errortitle ?>
 	<form id='register' action='register.php' method='post'
