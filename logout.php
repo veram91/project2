a7cfed7c4beb6f006161de156cf7da1ac8443484
suddin7 <suddin7@users.noreply.github.com>
2014-07-22 20:38:38 -0400

    Update logout.php
    just some more changes in css and wording...

--- logout.php
+++ logout.php
@@ -1,3 +1,4 @@
+
 <?php 
 session_start();
 session_destroy();
@@ -5,7 +6,8 @@ $_SESSION = array();
 if(isset($_COOKIE['username'])){
 	setcookie('username', '', 0);
 }
- $string="<br><h3 class='logoh1'>You are logged out of BlogNow successfully! have a great day.</h3><br/><a href='index.php'>Return to the Index Page</a>";
+ $string="<br><h3 class='logoh1'>You have been successfully logged out of BlogNow.<br> Have a great day!</h3><br/>";
+ $string2="<div class='return'><a style=align:'center' href='index.php'>Return to the Index Page</a></div>";
 ?>
 
 <html>
@@ -14,5 +16,6 @@ if(isset($_COOKIE['username'])){
 </head>
 <body>
 <?php echo $string ?>
+<?php echo $string2 ?>
 </body>
 </html>
